2024749529187a73dc5e8db4ce2a8c67ebc97c2c
Sergey Popov <quolpr@gmail.com>
2022-07-01 11:54:14 +0300

    Use @trong-orm/tauri-backend

--- src/App.tsx
+++ src/App.tsx
@@ -8,75 +8,10 @@ import {
   migrationsPlugin,
   reactiveQueriesPlugin,
 } from "@trong-orm/react";
-import React from "react";
-import SQLite from "tauri-plugin-sqlite-api";
+import { tauriBackend } from "@trong-orm/tauri-backend";
 
 import { List } from "./List";
 import { createNotesTableMigration } from "./migrations/createNotesTable";
-
-const tauriBackend =
-  (path: (dbName: string) => string): IDbBackend =>
-  ({ dbName, stopped$ }) => {
-    let db: SQLite | undefined = undefined;
-
-    return {
-      async initialize() {
-        db = await SQLite.open(path(dbName));
-
-        stopped$.subscribe(() => {
-          if (!db) {
-            console.error("Failed to stop DB‚ it is not initialized");
-
-            return;
-          }
-
-          // TODO: how to close db?
-        });
-      },
-      async execQueries(
-        queries: IQuery[],
-        opts: {
-          log: {
-            suppress: boolean;
-            transactionId?: string;
-          };
-        }
-      ) {
-        if (!db) {
-          throw new Error(
-            `Failed to run queries: ${queries
-              .map((q) => q.text)
-              .join(" ")}, db not initialized`
-          );
-        }
-
-        const result: IQueryResult[] = [];
-
-        for (const q of queries) {
-          const startTime = performance.now();
-
-          result.push(await db.select<IQueryResult>(q.text, q.values));
-
-          const end = performance.now();
-
-          if (!opts.log.suppress) {
-            console.info(
-              `[${dbName}]${
-                opts.log.transactionId
-                  ? `[tr_id=${opts.log.transactionId.slice(0, 6)}]`
-                  : ""
-              } ` +
-                queries.map((q) => q.text).join(" ") +
-                " Time: " +
-                ((end - startTime) / 1000).toFixed(4)
-            );
-          }
-        }
-
-        return result;
-      },
-    };
-  };
 
 const config: IInitDbClientConfig = {
   dbName: "helloWorld",
